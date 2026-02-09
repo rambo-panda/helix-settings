10d1a2f359fff1b1d80f13556ed92bde9ed6aca3
rambo <luwei.li@17zuoye.com>
2026-02-09 14:27:52 +0800

    feat: init

---
+++ build-config.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+output_file="config.toml"
+
+> "$output_file"
+
+files_to_merge=(
+    "config.base.toml"
+    "vim.keymapping.toml"
+)
+
+for file in "${files_to_merge[@]}"; do
+    if [ -f "$file" ]; then
+        echo "# From file: $file" >> "$output_file"
+        cat "$file" >> "$output_file"
+        echo -e "\n" >> "$output_file"
+    else
+        echo "Warning: File $file not found, skipping..." >&2
+    fi
+done
